c52c2e9fa94e1d9d9dbafb7393feddfe28374d77
Jakob Højgård Olsen <jho@grafect.com>
2013-12-11 16:43:25 +0100

    Updated code so it works with the pololu motorcontroller

--- BLE_motor_sketch/BLE_motor_sketch.ino
+++ BLE_motor_sketch/BLE_motor_sketch.ino
@@ -3,18 +3,24 @@
 #include <boards.h>
 #include <ble_shield.h>
 #include <services.h>
-#include "Boards.h"
 
 
 
-#define I_InA1            10                  // INA motor pin
-#define I_InB1            9                    // INB motor pin 
-#define I_PWM1            8                   // PWM motor pin
 
-#define II_InA1            13                  // INA motor pin
-#define II_InB1            12                  // INB motor pin 
-#define II_PWM1            11                   // PWM motor pin
+#define I_InA1            6                  // INA motor pin
+#define I_InB1            5                    // INB motor pin 
+#define I_PWM1            4                   // PWM motor pin
 
+#define II_InA1            3                  // INA motor pin
+#define II_InB1            2                  // INB motor pin 
+#define II_PWM1            1                   // PWM motor pin
+
+ char fb;
+ char lr;
+  int I_PWM_val = 0;
+  int II_PWM_val =0;
+  int steeringActive = 0;
+  int throttleActive = 0;
 void setup() {
 
   
@@ -27,7 +33,9 @@ void setup() {
   pinMode(II_InA1, OUTPUT);
   pinMode(II_InB1, OUTPUT);
   pinMode(II_PWM1, OUTPUT); 
-
+  Serial.begin(57600);
+ 
+  
   // put your setup code here, to run once:
   SPI.setDataMode(SPI_MODE0);
   SPI.setBitOrder(LSBFIRST);
@@ -36,18 +44,118 @@ void setup() {
 
   ble_begin();
 
-  Serial.begin(57600);
+ Serial.println("Forward = 1 / Backwards = 2 / or 'x' to clear");
+  
+
+}
+
+
+
+
+void loop() {
+
+  
+  
+  while(ble_available()){
+       //byte motor1;
+    byte valForMotor1;
+    byte dirForMotor1;
+ 
+    //byte motor2;
+    byte valForMotor2;
+    byte dirForMotor2;
+
+    if(valForMotor1 = ble_read()){
+     valForMotor1 = ble_read();
+     dirForMotor1 = ble_read();
+     valForMotor2 = ble_read();
+     dirForMotor2 = ble_read();
+
+
+   if (throttleActive == 1){
+     fb = 'x';
+     throttleActive = 0;
+    }
+    
+     
+    if(valForMotor1 != 99){
+      throttleActive = 1;
+      if(valForMotor1 == 255){
+      fb = '1';
+      } if(valForMotor1 == 0){
+        fb = '2';
+      }
+    }
+  
+ if(steeringActive == 1){
+    lr='s';
+    steeringActive = 0;
+    }
+
+
+     if(valForMotor2 != 99){
+       steeringActive = 1;
+       if(valForMotor2 == 255){
+      lr = '4';
+      } 
+      if(valForMotor2 == 0){
+      lr = '6';
+      }
+}
+  
+   
+ 
+// FORWARD BACKWARD //
+
+    Serial.println(fb);
+    if (fb >= '0' && fb <= '1')
+    {
+     motorForward(255);                        
+    }
+    if (fb >= '2' && fb <= '3')
+    {
+     motorBackward(255);
+   
+    }
+    if (fb == 'x')
+    {
+    motorStop();
+    Serial.println("throttlestop");
+    }
+   }
+    
+    
+    // LEFT RIGHT //
+     Serial.println(lr);
+     if (lr >= '4' && lr <= '5')
+    {
+     motorIIBackward(255);
+    }
+      if (lr >= '6' && lr <= '7')
+    {
+   motorIIForward(255);
+    }
+  if (lr == 's')
+    {
+    motorIIStop();
+      Serial.println("steering stop");
+    }
+    }
+
+ble_do_events();
 }
 
 void motorForward(int I_PWM_val)  {
   analogWrite(I_PWM1, I_PWM_val);
   digitalWrite(I_InA1, LOW);
   digitalWrite(I_InB1, HIGH);
+  Serial.println("motor forward");
 }
 void motorIIForward(int II_PWM_val)  {
   analogWrite(II_PWM1, II_PWM_val);
   digitalWrite(II_InA1, LOW);
   digitalWrite(II_InB1, HIGH);
+  Serial.println("motor left");
 }
 
 
@@ -55,12 +163,14 @@ void motorBackward(int I_PWM_val)  {
   analogWrite(I_PWM1, I_PWM_val);
   digitalWrite(I_InA1, HIGH);
   digitalWrite(I_InB1, LOW);
+  Serial.println("motor backward");
 }
 
 void motorIIBackward(int II_PWM_val)  {
   analogWrite(II_PWM1, II_PWM_val);
   digitalWrite(II_InA1, HIGH);
   digitalWrite(II_InB1, LOW);
+  Serial.println("motor right");
 }
 
 void motorStop()  {
@@ -73,114 +183,6 @@ void motorIIStop()  {
   analogWrite(II_PWM1, 0);
   digitalWrite(II_InA1, LOW);
   digitalWrite(II_InB1, LOW);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly: 
-
-
-  if(ble_connected()){
-   // digitalWrite(7,255); 
-  }  
-
-   
-
-  
-  while(ble_available()){
-    
-    
-    
-    
-    //byte motor1;
-    byte valForMotor1;
-    byte dirForMotor1;
-    
-    //byte motor2;
-    byte valForMotor2;
-    byte dirForMotor2;
-    
-    if(valForMotor1 = ble_read()){
-      //read the first number for the led to address, and the second for the value...
-
-     dirForMotor1 = ble_read();
-     
-     valForMotor2 = ble_read();
-     dirForMotor2 = ble_read();
-     
-     //digitalWrite(dir_a, dirForMotor1);
-     //analogWrite(pwm_a, valForMotor1);
-     
-     //digitalWrite(dir_b, dirForMotor2);
-     //analogWrite(pwm_b, valForMotor2);
-    
-
-
-     //Serial.print("dirForMotor1:");     
-     //Serial.println(dirForMotor1);
-
-      
-     //Serial.print("valForMotor1:");     
-     //Serial.println(valForMotor1);
-      
-     //Serial.print("valForMotor2:");     
-     //Serial.println(valForMotor2);
-     
-     //Serial.print("dirForMotor2:");     
-     //Serial.println(dirForMotor2);
-
-      //Serial.println("-----");
-
-//Serial.println(valForMotor1);
-//Serial.println(valForMotor2);
-// Go farward
-if(valForMotor1 >= 240){
-Serial.println("Going forward");
-     motorForward(255);                        
-    }else{
-    motorStop();
-    motorIIStop();
-    }
-if(valForMotor1 <= 50){
-Serial.println("Going backwards");
- motorBackward(255);   
-    }else{
-    motorStop();
-    motorIIStop();
-    } 
-   if(valForMotor2 >= 240){
-Serial.println("Left");
-//motorIIForward(100);   
-    }else{
-    motorStop();
-    motorIIStop();
-    }
-   if(valForMotor2 <= 50){
-Serial.println("Right");
-//motorIIBackward(100);
-    } else{
-    motorStop();
-    motorIIStop();
-    }
-    
-  }
-  }
-  /*
-  if(digitalRead(4) == LOW){
-      Serial.println("HI");
-       ble_write('1'); 
-    }
-  */
-  if (!ble_connected())
-  {
-    digitalWrite(7, LOW);
-
-    //analog_enabled = false;
-    //digitalWrite(DIGITAL_OUT_PIN, LOW);
-  }
-  
-  // Allow BLE Shield to send/receive data
-  ble_do_events();
-  //digitalWrite(RED, LOW);
 }
 
 
